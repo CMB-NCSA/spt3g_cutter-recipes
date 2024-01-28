6fbcd95a77775296f1008c6f99d7f2ce48f9a265
Felipe Menanteau <felipe@illinois.edu>
2024-01-28 10:55:17 -0300

    Adding build script

---
+++ build_docker.sh
@@ -0,0 +1,11 @@
+# Specify the version of spt3g_cutter to build
+export SPT3G_CUTTER_VERSION=0.4.0
+export IMAGE_REPO=registry.gitlab.com/spt3g/kubernetes/spt3g_cutter
+
+# Build the Docker image
+docker build -t ${IMAGE_REPO}:${SPT3G_CUTTER_VERSION} \
+    --build-arg SPT3G_CUTTER_VERSION \
+    -f docker/Dockerfile .
+
+echo 'Push commands:'
+echo "   docker push ${IMAGE_REPO}:${SPT3G_CUTTER_VERSION}"
