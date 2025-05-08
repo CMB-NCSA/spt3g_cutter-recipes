32a69dc998855d82d26fe506e14ced0f86a2fdfc
Felipe Menanteau <felipe@illinois.edu>
2025-05-08 17:06:55 -0500

    Fixes to build scripts

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,5 @@
 # Specify the version of spt3g_cutter to build
-export SPT3G_CUTTER_VERSION=0.4.3
+export SPT3G_CUTTER_VERSION=0.4.4
 export IMAGE_REPO=registry.gitlab.com/spt3g/kubernetes/spt3g_cutter
 
 # Build the Docker image
---
+++ docker/setSPTEnv.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+echo "Adding spt3g_cutter"
+source /opt/spt/spt3g_cutter/setpath.sh /opt/spt/spt3g_cutter
