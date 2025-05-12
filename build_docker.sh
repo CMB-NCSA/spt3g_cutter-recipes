ec69ee1ff6774cc2ba2e9d8d4cb86d4f7ee7929d
Felipe Menanteau <felipe@illinois.edu>
2025-05-12 14:25:52 -0500

    TAG version 0.4.5

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,5 @@
 # Specify the version of spt3g_cutter to build
-export SPT3G_CUTTER_VERSION=0.4.4
+export SPT3G_CUTTER_VERSION=0.4.5
 export IMAGE_REPO=registry.gitlab.com/spt3g/kubernetes/spt3g_cutter
 
 # Build the Docker image
